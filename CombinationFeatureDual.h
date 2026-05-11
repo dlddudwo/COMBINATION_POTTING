17585b6bb5c268f6736ca58db35f4830959c571d
Lee YoungJae <zic11234@naver.com>
2026-05-11 16:55:29 +0900

    Create CombinationFeatureDual.h

---
+++ CombinationFeatureDual.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "FuncAlgorithm.h"
+
+class CCombinationFeatureDual
+{
+public:
+	// root recipe
+	virtual Json::Value Run(Json::Value a_recipe, Json::Value& a_result_json);
+
+private:
+};
+
